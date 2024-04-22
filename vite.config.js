f13d4280ef7404fa50888abd02c12569df67df8c
Janphan <janphan061@gmail.com>
2024-04-22 20:26:20 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/customer_training/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
