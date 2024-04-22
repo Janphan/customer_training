73e0af2309c60046d9d39f4d6eb346a109dda4ea
Janphan <janphan061@gmail.com>
2024-04-22 19:53:44 +0300

    map training to calendar event

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -5,21 +5,44 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment"; // Use moment for localizer
 import Training from "./training";
 import { getTraining, getCustomers } from "../customerTrainingAPI";
+
 const localizer = momentLocalizer(moment); // Set localizer
 
-const CalendarComponent = ({ training }) => {
-    
+function TrainingCalendar () {
+    const [trainingData, setTrainingData] = useState([]);
+    const [calendarEvents, setCalendarEvents] = useState([]);
+
+    const mapToCalendar = (trainings) => {
+        return trainings.map((training) => ({
+          title: `${training.activity} / ${training.customer.firstname} ${training.customer.lastname} `,
+          start: new Date(training.date),
+          end: moment(training.date)
+            .add(training.duration, 'minutes')
+            .toDate(),
+        }))
+      }
+
+    useEffect(() => {
+        getTraining()
+      .then(data => {
+        setTrainingData(data)
+        setCalendarEvents(mapToCalendar(data))
+      })
+      .catch(error => console.error('Failed fetching trainings:', error))
+  }, [])
+
   return (
     <div>
       <BigCalendar
         localizer={localizer}
-        events={training} // Make sure training is an array of event objects
+        events={calendarEvents} // Make sure training is an array of event objects
         startAccessor="start" // Adjust based on your event object
         endAccessor="end" // Adjust based on your event object
+        views={['month', 'week', 'day']}
         style={{ height: 500 }} // Set a height to prevent overflow
       />
     </div>
   );
 };
 
-export default CalendarComponent;
+export default TrainingCalendar;
