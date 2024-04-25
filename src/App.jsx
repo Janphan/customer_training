ea20d37157fe766fef20ac2d24838f6f433312ce
Janphan <janphan061@gmail.com>
2024-04-25 21:34:17 +0300

    edit navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,19 @@ import Container from "@mui/material/Container";
 
 import SearchAppBar from "./SearchAppbar";
 import MenuDropdown from "./MenuDropdown";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Customerlist from "./components/customerlist";
-import Training from  "./components/training";
+import Training from "./components/training";
 import Calendar from "./components/Calendar";
 import AnalysisTraining from "./components/AnalysisTraining";
 function App() {
   return (
     <>
-      <Container maxWidth="xl">
-        <SearchAppBar />
+      <Container maxWidth="xl">        
         <Router>
           {" "}
-          {/* This component wraps all the routing components */}
-          <MenuDropdown /> {/* Your Drawer Component */}
+          <SearchAppBar />
           <Routes>
             {/* Define individual routes and map them to components */}
             <Route path="/Customerlist" element={<Customerlist />} />
--- src/MenuDropdown.jsx
+++ src/MenuDropdown.jsx
@@ -1,49 +1,52 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';  // Import Link from react-router-dom
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import GroupIcon from '@mui/icons-material/Group';
-import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import BarChartIcon from '@mui/icons-material/BarChart';
+import * as React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import GroupIcon from "@mui/icons-material/Group";
+import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import BarChartIcon from "@mui/icons-material/BarChart";
 
-export default function MenuDropdown() {
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function MenuDropdown(props) {
   const [open, setOpen] = React.useState(false);
-
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
-  };
 
   // Define icons
   const icons = {
     Customer: <GroupIcon />,
     Training: <DirectionsRunIcon />,
     Calendar: <CalendarMonthIcon />,
-    Statistics: <BarChartIcon />
+    Statistics: <BarChartIcon />,
   };
 
   // Define list items with corresponding icons and routes
   const listItems = [
-    { text: 'Customer', icon: icons.Customer, route: '/customerlist' },
-    { text: 'Training', icon: icons.Training, route: '/training' },
-    { text: 'Calendar', icon: icons.Calendar, route: '/Calendar' },
-    { text: 'Statistics', icon: icons.Statistics, route: '/AnaylysisTraining' },
+    { text: "Customer", icon: icons.Customer, route: "/customerlist" },
+    { text: "Training", icon: icons.Training, route: "/training" },
+    { text: "Calendar", icon: icons.Calendar, route: "/Calendar" },
+    { text: "Statistics", icon: icons.Statistics, route: "/AnalysisTraining" },
   ];
 
   // Create the Drawer content with List and navigation links
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+    <Box sx={{ width: 250 }} role="presentation">
       <List>
         {listItems.map((item) => (
           <ListItem key={item.text} disablePadding>
-            <Link to={item.route} style={{ textDecoration: 'none', color: 'inherit' }}>  {/* Link to route */}
+            <Link
+              to={item.route}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              {" "}
+              {/* Link to route */}
               <ListItemButton>
                 <ListItemIcon>{item.icon}</ListItemIcon>
                 <ListItemText primary={item.text} />
@@ -57,11 +60,20 @@ export default function MenuDropdown() {
   );
 
   return (
-    <div>
-      <Button onClick={toggleDrawer(true)}>Open drawer</Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
+    <>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="open drawer"
+        sx={{ mr: 2 }}
+        onClick={() => setOpen(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer open={open} onClose={() => setOpen(false)}>
         {DrawerList}
       </Drawer>
-    </div>
+    </>
   );
 }
